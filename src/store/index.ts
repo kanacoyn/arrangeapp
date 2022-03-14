1eea15a0b1513093d94429e529a44768706b9485
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-03-14 14:42:37 +0900

    getter設置

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,24 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import { Event } from "@/types/event";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    event: new Event("", "", [], "", "", ""),
   },
-  mutations: {
+  mutations: {},
+  actions: {},
+  getters:{
+    /**
+     * 候補日程を取得する.
+     * @param state - ステート
+     * @returns 候補日程
+     */
+    getDateList(state){
+      return state.event.date;
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  modules: {},
+});
