8ec70a720ca5c161e9920c452cd3c42ec8c83c00
kanacoyn <knkyn723@gmail.com>
2022-03-14 15:14:29 +0900

    イベント記入欄を作成（エラーメッセージは未記載)遷移ボタン

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -62,6 +62,64 @@
                       />
                     </td>
                   </tr>
+
+                  <tr>
+                    <th>パスワード設定</th>
+                    <td>
+                      ※イベントを閲覧する場合のパスワードを設定できます。
+                      <br />
+                      <input
+                        type="radio"
+                        name="pw"
+                        id="pw0"
+                        value="0"
+                        checked=""
+                      />
+                      <label for="pw0">設定しない</label><br />
+                      <input type="radio" name="pw" id="pw1" value="1" />
+                      <label for="pw1">設定する</label><br />
+                      <input
+                        type="text"
+                        name="password"
+                        maxlength="8"
+                        style="ime-mode: disabled; width: 100px"
+                        value=""
+                      />
+                      (英数字8文字以内)
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th>回答選択肢</th>
+                    <td>
+                      <input
+                        type="radio"
+                        name="eventchoice"
+                        id="eventchoice1"
+                        value="1"
+                        checked=""
+                      />
+                      <label for="eventchoice1">「○△×」から選択</label><br />
+
+                      <input
+                        type="radio"
+                        name="eventchoice"
+                        id="eventchoice2"
+                        value="2"
+                      />
+                      <label for="eventchoice2">「○×」から選択</label>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th align="center">
+                      <input
+                        type="button"
+                        value="次に進む"
+                        onclick="confirmEvent"
+                        style="width: 200px"
+                      />
+                    </th>
+                  </tr>
                 </table>
               </div>
             </form>
@@ -75,7 +133,11 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class XXXComponent extends Vue {}
+export default class XXXComponent extends Vue {
+  confirmEvent() {
+    this.$router.push("/confirmEvent");
+  }
+}
 </script>
 
 <style scoped></style>
