8cf0167d34cc297f23891d75cb82f9e49943fb48
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-03-24 16:57:14 +0900

    微修正

--- src/components/CompSelectBox.vue
+++ src/components/CompSelectBox.vue
@@ -22,9 +22,11 @@ import { Component, Emit, Prop, Vue } from "vue-property-decorator";
 export default class XXXComponent extends Vue {
   // 親コンポーネントから受け取る日付ID
   @Prop()
-  private dateId!: number;
+  private timeId!: number;
   // 日程回答
   private selectAnswer = "0";
+
+  private initFlag?:boolean;
 
   @Emit()
   selectItem(answer: string): void {
