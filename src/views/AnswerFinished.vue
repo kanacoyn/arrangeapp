8ad24cdfdf0cc8841a2a76f80756b164167c8de2
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-03-25 09:31:59 +0900

    テーブルコピペ

--- src/views/AnswerFinished.vue
+++ src/views/AnswerFinished.vue
@@ -1,17 +1,51 @@
 <template>
   <div>
-    
+    <table align="center">
+      <tr>
+        <th></th>
+        <th>第1候補</th>
+        <th>第2候補</th>
+        <th>第3候補</th>
+      </tr>
+      <tr class="total">
+        <td>合計</td>
+        <td v-for="count of currentAnswerCount" v-bind:key="count.id">
+          {{ count }}
+        </td>
+      </tr>
+      <tr v-for="user of userList" v-bind:key="user.userId">
+        <td>
+          <router-link v-bind:to="'/userAnswer/' + user.userId">{{
+            user.name
+          }}</router-link>
+        </td>
+        <td v-for="answer of user.registerAnswer" v-bind:key="answer.id">
+          {{ answer }}
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class XXXComponent extends Vue {
-
-}
+export default class XXXComponent extends Vue {}
 </script>
 
 <style scoped>
+th,
+td {
+  border: solid 1px;
+  padding: 10px;
+}
 
+table {
+  border-collapse: collapse;
+  margin-bottom: 30px;
+}
+
+.total {
+  background-color: lavenderblush;
+}
 </style>
