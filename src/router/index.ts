840cc44390feb2e48e7a5f1c9d50e9c9288b8f1e
kanacoyn <knkyn723@gmail.com>
2022-03-30 12:00:17 +0900

    備忘、感想を追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,10 @@ const routes: Array<RouteConfig> = [
     path: "/answerQuestions",
     component: () => import("../views/AnswerQuestions.vue"),
   },
+  {
+    path: "/kanako",
+    component: () => import("../views/Kanako.vue"),
+  },
 
   {
     path: "/event",
